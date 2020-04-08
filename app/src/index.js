dac515d835d6c77ea981bcb17b02b8b5249964e6
yaobin <184821967@qq.com>
2020-04-08 11:28:32 +0800

    前端框架搭建

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.scss';
+// import * as serviceWorker from './serviceWorker';
+// import App from './router/index';
+// import { Provider } from 'react-redux'
+// import store from './store'
+//
+// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+//
+// serviceWorker.unregister();
+
+ReactDOM.render(<h1>hi</h1>, document.getElementById('root'));
