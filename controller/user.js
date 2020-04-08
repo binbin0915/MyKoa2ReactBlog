0a23a081814545580713de7961f09c04d961b644
yaobin <184821967@qq.com>
2020-04-08 10:47:39 +0800

    后台框架搭建

---
+++ controller/user.js
@@ -0,0 +1,95 @@
+const { userModel} = require('../model')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
+const { passwordHash, passwordCompare } = require('../lib/bcrypt')
+const { createToken, getUserInfo } = require('../lib/token')
+class userController {
+    // 用户登录
+    // 用户登录
+    static async login(ctx) {
+        let { username, password, type } = ctx.request.body
+        let where = {}
+        if (type === 'admin') {
+            where = { username, auth: 1 }
+        } else {
+            where = { username }
+        }
+        let response = await userModel.findOne({ where })
+        if (response) {
+            // let isSame = await passwordCompare(password, response.password)  // 校验密码是否正确
+            let isSame =true;
+            if (isSame) {
+                let { id, username, name, auth } = response
+                let token = await createToken({ id, username, auth })
+                ctx.body = {
+                    status: 1,
+                    message: '登录成功',
+                    response: { id, username, name, token }
+                }
+            } else {
+                ctx.body = {
+                    status: 0,
+                    message: '密码不正确'
+                }
+            }
+        } else {
+            if (type === 'admin') {
+                ctx.body = {
+                    status: 0,
+                    message: '管理员账号不存在'
+                }
+            } else {
+                ctx.body = {
+                    status: 0,
+                    message: '用户不存在'
+                }
+            }
+        }
+    }
+
+    // 用户注册
+    // 校验用户名是否已经注册，密码加密处理
+    static async register(ctx) {
+        let params = ctx.request.body
+        let { username, password, name } = params
+        let user = await userModel.findOne({ where: { username } }) // 校验是否用户名是否已经注册
+        if (user) {
+            ctx.body = {
+                status: 0,
+                message: "此用户名已注册过"
+            }
+        } else {
+            let bcryptPassword = await passwordHash(password)  // 密码加密
+            let response = await userModel.create({ username, name, password: bcryptPassword })
+            if (response) {
+                ctx.body = {
+                    status: 1,
+                    message: '注册成功'
+                }
+            } else {
+                ctx.body = {
+                    status: 0,
+                    message: '注册失败'
+                }
+            }
+        }
+    }
+    // 检验用户名是否已经注册
+    static async checkUsername(ctx) {
+        let { username } = ctx.request.body
+        let response = await userModel.findOne({ where: { username } })
+        if (response) {
+            ctx.body = {
+                status: 0,
+                message: '此电子邮箱已经被注册',
+            }
+        } else {
+            ctx.body = {
+                status: 1,
+                message: '此电子邮箱可用'
+            }
+        }
+
+    }
+}
+module.exports = userController
---
+++ router/index.js
@@ -0,0 +1,5 @@
+const Router = require('koa-router')
+const router = new Router({ prefix: '/api' })
+const userRouter = require('./user')
+router.use('/user', userRouter.routes())
+module.exports = router
